68064ef59e3747aae310e8aa66752b753d7a0d4a
theharold9000 <harold@justdigitalpeople.com.au>
2017-10-15 21:03:29 +1000

    New and improved!

--- ES6 Functional Job Ad.js
+++ ES6 Functional Job Ad.js
@@ -1,8 +1,8 @@
 const jobInfo = {
-  companyInfo: 'Highly successful company',
-  serverSideStack: 'Massive and modern platform (.NET 4.6.2 codebase is considered the \'ancient legacy stuff\')',
-  clientSideStack: 'React/Redux',
-  companyCulture: 'The team value TDD (they can tell you test coverage on both legacy and new code with single-digit percentage precision'
+  companyInfo: 'Highly successful and popular multi-national brand',
+  serverSideStack: 'Dockerized micro-services, built in Java 8',
+  clientSideStack: 'Angular/TypeScript',
+  companyCulture: 'A company that solves global-scale technical problems and values innovation'
 }
 
 const emailTo = console.log
@@ -10,22 +10,21 @@ const emailTo = console.log
 const hasSkill = (skill, value) => (skillset) => skillset.includes(skill) ? value : 0
 
 const mustHaves = [
-  hasSkill('NET Core 1.0+', 1),
-  hasSkill('React/Redux experience (commercial or recreational)', 1),
-  hasSkill('Cloud computing (preferably MS Azure', 1),
+  hasSkill('Core Java 7, 8 or 9', 1),
+  hasSkill('Spring Boot or something similar', 1),
+  hasSkill('Cloud computing (preferably AWS', 1),
   hasSkill('TDD (what\'s your test coverage on new code like atm?)', 1),
-  hasSkill('Understanding of time/space complexity (you will be interrogated on this)', 1)
 ]
 
 const bonusRound = [
   hasSkill('Fable, Elm or Reason', 2),
-  hasSkill('Node, Express and Meteor', 2)
+  hasSkill('Docker, Kubernetes or similar technologies', 2)
 ]
 
 const skillset = [
-  'React/Redux experience (commercial or recreational)',
+  'Core Java 7, 8 or 9',
+  'Spring Boot or something similar',
   'TDD (what\'s your test coverage on new code like atm?)',
-  'Understanding of time/space complexity (you will be interrogated on this)',
   'Node, Express and Meteor',
   'Fable, Elm or Reason'
 ]

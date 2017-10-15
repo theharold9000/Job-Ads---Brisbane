b807fe9a300a839d30eea0c15f7f1fdeb042eb30
theharold9000 <harold@justdigitalpeople.com.au>
2017-10-15 21:07:23 +1000

    Added footer

--- ES6 Functional Java Developer.js
+++ ES6 Functional Java Developer.js
@@ -37,3 +37,15 @@ if (rating >= 5) {
 } else {
   console.log('Keep trying!')
 }
+/*
+See something dumb or incorrect in one of my ads? Let me know! Constructive criticism is always appreciated.
+
+If you have an active GitHub, GitLab, BitBucket or Stack Overflow profile include a link or follow me because I want to see it!
+
+**Harold Liu**</br>
+**ph/txt:** 0400 659 332 | **email:** harold@justdigitalpeople.com.au</br>
+**Twitter:** @theharold9000</br>
+**Youtube:** https://www.youtube.com/user/justdigitalpeople</br>
+**Instagram:** justdigitalpeople</br>
+**GitHub:** theharold9000</br>
+*/

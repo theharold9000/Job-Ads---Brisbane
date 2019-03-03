316c265b33a41a686e2274bddb3c8e2b48ac79f9
theharold9000 <hanming1@hotmail.com>
2019-03-04 08:24:02 +1000

    Updates and stuff

--- ES6 Functional Java Developer.js
+++ ES6 Functional Java Developer.js
@@ -10,8 +10,8 @@ const emailTo = console.log
 const hasSkill = (skill, value) => (skillset) => skillset.includes(skill) ? value : 0
 
 const mustHaves = [
-  hasSkill('Core Java 7, 8 or 9', 1),
-  hasSkill('Spring Boot or something similar', 1),
+  hasSkill('Java 8 or 9', 1),
+  hasSkill('Spring', 1),
   hasSkill('Cloud computing (preferably AWS)', 1),
   hasSkill('TDD (what\'s your test coverage on new code like atm?)', 1),
 ]
@@ -33,7 +33,7 @@ const rating = [...mustHaves, ...bonusRound]
   .reduce((tally, skill) => tally + skill(skillset), 0)
 
 if (rating >= 5) {
-  emailTo('harold@justdigitalpeople.com.au')
+  emailTo('harold@blackroc.co')
 } else {
   console.log('Keep trying!')
 }
@@ -43,9 +43,6 @@ See something dumb or incorrect in one of my ads? Let me know! Constructive crit
 If you have an active GitHub, GitLab, BitBucket or Stack Overflow profile include a link or follow me because I want to see it!
 
 **Harold Liu**</br>
-**ph/txt:** 0400 659 332 | **email:** harold@justdigitalpeople.com.au</br>
-**Twitter:** @theharold9000</br>
-**Youtube:** https://www.youtube.com/user/justdigitalpeople</br>
-**Instagram:** justdigitalpeople</br>
-**GitHub:** theharold9000</br>
+**ph/txt:** 0400 659 332 | **email:** harold@blackroc.co/br>
+**Twitter:** @theharold9000 | **GitHub:** theharold9000</br>
 */
